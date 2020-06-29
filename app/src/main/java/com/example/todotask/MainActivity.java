3854e374ad6b7827e2275dfdec9b4b8e7e8107ee
AlphaMohit07 <56122611+AlphaMohit07@users.noreply.github.com>
2020-06-29 18:02:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todotask/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.todotask;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.todotask.data.DatabaseHandler;
+import com.example.todotask.model.Item;
+import com.example.todotask.util.ListActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private AlertDialog.Builder builder;
+    private AlertDialog dialog;
+    private Button saveButton;
+    private EditText babyItem;
+    private EditText itemQuantity;
+    private EditText itemColor;
+    private EditText itemSize;
+    private DatabaseHandler databaseHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        databaseHandler = new DatabaseHandler(this);
+
+        byPassActivity();
+
+        //check if item was saved
+        List<Item> items = databaseHandler.getAllItems();
+/*        for (Item item : items) {*/
+/*            Log.d("Main", "onCreate: " + item.getItemColor());*/
+/*        } */
+
+
+
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                createPopupDialog();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void byPassActivity() {
+        if (databaseHandler.getItemsCount() > 0) {
+            startActivity(new Intent(MainActivity.this, ListActivity.class));
+            finish();
+        }
+    }
+
+    private void saveItem(View view) {
+        //Todo: save each baby item to db
+        Item item = new Item();
+
+        String newItem = babyItem.getText().toString().trim();
+        String newColor = itemColor.getText().toString().trim();
+        String quantity = itemQuantity.getText().toString().trim();
+        String size = itemSize.getText().toString().trim();
+
+        item.setItemName(newItem);
+        item.setItemColor(newColor);
+        item.setItemQuantity(quantity);
+        item.setItemSize(size);
+
+        databaseHandler.addItem(item);
+
+        Snackbar.make(view, "Item Saved",Snackbar.LENGTH_SHORT)
+                .show();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //code to be run
+                dialog.dismiss();
+                //Todo: move to next screen - details screen
+                startActivity(new Intent(MainActivity.this, ListActivity.class));
+
+            }
+        }, 1200);// 1sec
+    }
+
+    private void createPopupDialog() {
+
+        builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup, null);
+
+        babyItem = view.findViewById(R.id.babyItem);
+        itemQuantity = view.findViewById(R.id.itemQuantity);
+        itemColor = view.findViewById(R.id.itemColor);
+        itemSize = view.findViewById(R.id.itemSize);
+        saveButton = view.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!babyItem.getText().toString().isEmpty()
+                        && !itemColor.getText().toString().isEmpty()
+                        && !itemQuantity.getText().toString().isEmpty()
+                        && !itemSize.getText().toString().isEmpty()) {
+                    saveItem(v);
+                }else {
+                    Snackbar.make(v, "Empty Fields not Allowed", Snackbar.LENGTH_SHORT)
+                            .show();
+                }
+
+            }
+        });
+
+        builder.setView(view);
+        dialog = builder.create();// creating our dialog object
+        dialog.show();// important step!
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/todotask/model/Item.java
@@ -0,0 +1,71 @@
+package com.example.todotask.model;
+
+import java.sql.Time;
+
+public class Item {
+    private int id;
+    private String itemName;
+    private String itemColor;
+    private String itemQuantity;
+    private String itemSize;
+    private String dateItemAdded;
+
+    public Item() {
+    }
+
+    public Item(String itemName, String itemColor, String itemQuantity, String itemSize, String dateItemAdded) {
+        this.itemName = itemName;
+        this.itemColor = itemColor;
+        this.itemQuantity = itemQuantity;
+        this.itemSize = itemSize;
+        this.dateItemAdded = dateItemAdded;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemColor() {
+        return itemColor;
+    }
+
+    public void setItemColor(String itemColor) {
+        this.itemColor = itemColor;
+    }
+
+    public String getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(String itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public String getItemSize() {
+        return itemSize;
+    }
+
+    public void setItemSize(String itemSize) {
+        this.itemSize = itemSize;
+    }
+
+    public String getDateItemAdded() {
+        return dateItemAdded;
+    }
+
+    public void setDateItemAdded(String dateItemAdded) {
+        this.dateItemAdded = dateItemAdded;
+    }
+}
